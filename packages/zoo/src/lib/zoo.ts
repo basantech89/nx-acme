22ef923920d5939b9f518477afe165d3ca198b7d
Basant Soni <basantech89@gmail.com>
2025-11-27 20:07:51 +0530

    feat(utils): somsomso

--- packages/zoo/src/lib/zoo.ts
+++ packages/zoo/src/lib/zoo.ts
@@ -2,6 +2,8 @@ import { getRandomAnimal } from '@nx-acme/animal'
 import { formatMessage } from '@nx-acme/util'
 
 export function zoo(): string {
+	console.log('zoo 2')
+
 	const result = getRandomAnimal()
 	const message = `${result.name} says ${result.sound}!`
 	return formatMessage('ZOO', message)
