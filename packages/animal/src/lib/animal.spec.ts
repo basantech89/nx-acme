f4c9793c5471555ed6a91e7fb02cdc9e8ea51c22
Basant Soni <basantech89@gmail.com>
2025-11-27 11:15:30 +0530

    feat(animal): change to 8

--- packages/animal/src/lib/animal.spec.ts
+++ packages/animal/src/lib/animal.spec.ts
@@ -2,6 +2,6 @@ import { animal } from './animal'
 
 describe('animal', () => {
 	it('should work', () => {
-		expect(animal()).toEqual('animal7')
+		expect(animal()).toEqual('animal8')
 	})
 })
--- packages/animal/src/lib/animal.ts
+++ packages/animal/src/lib/animal.ts
@@ -1,7 +1,7 @@
 import { getRandomItem } from '@nx-acme/util'
 
 export function animal(): string {
-	return 'animal7'
+	return 'animal8'
 }
 
 export interface Animal {
